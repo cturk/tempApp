16b6fb5603a90a2e450a202667f6c15ae284a08a
Cody Turk <codyturk@gmail.com>
2018-05-03 11:01:06 -0500

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log('hello');
+var cTemp = prompt("What is the temperature in celsius?"); // celsius temperature
+var fTemp = ((cTemp * 9) /5 ) + 32; // fahrenheight temperature
+document.write("Temperature in Celsius: " + cTemp + " degrees<br/>")
+document.write("Temperature in Fahrenheight: " + fTemp + " degrees")
